4f7a22e496fe41c31b31b4f86c6ad9195e80cbf3
Apoorv Kothari <apoorv@toidiu.com>
2024-07-25 17:45:58 -0700

    some rough outline of the project

--- README.md
+++ README.md
@@ -13,14 +13,25 @@ Based on TIKV impl: https://github.com/tikv/raft-rs
 > - State Machine, the place to save the user data;
 > - Transport, the network layer for communication.
 
+`struct StateMachine`
+
 "Replicated state machine". The concept that the same data is spread over many
 machines so the failure of minority of machines doesnt impact liveliness.
 
+`struct ReplicatedLog`
+
 Replicated state machines are implemented via a "replicated log", which are a serive of
 commands to be executed in-order.
+
+`struct ConsensusAlgo`
 
 The job of the "consensus algorithm" is keeping the replicated log consistent.
 
 The consensus algo on a server receives commands from a client and adds them it its
 log. It then commicumates with other consensus algo on other servers to ensure that
 every log contains the same commands in the same order.
+
+3 properties
+- Leader Election
+- Log Repliation: leader manages replicated log
+- Safety: entries added to the state machine are absolute and correct
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
+#![allow(unused)]
+
 fn main() {
     println!("Hello, world!");
+}
+
+enum State {
+    Follower,
+    Leader,
+    Candidate,
+}
+
+// A term is a logical clock
+struct Term(usize);
+
+struct CandidateId(usize);
+
+struct LogEntry {
+    term: Term,
+    command: Command,
+}
+
+struct Server {
+    id: CandidateId,
+    state: State,
+
+    // ==== persistent state
+    current_term: Term,
+    voted_for: Option<CandidateId>,
+    log: Vec<LogEntry>,
+
+    // ==== volatile state
+    // idx of highest log entry known to be committed
+    commit_idx: usize,
+    // idx of the highest log entry applied to the state machine
+    last_applied: usize,
+
+    // ==== volatile state on leaders
+    // for each server, idx of next log entry to send to that server
+    next_idx: usize,
+    // for each server, idx of highest log entry known to be replicated on server
+    match_idx: usize,
+}
+
+enum Command {
+    // Leader election
+    RequestVote,
+
+    // Add entries and heartbeat
+    AppendEntries,
 }
