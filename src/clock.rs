66a3bd073138452dff281acc68a08e7a3ba78b4d
Apoorv Kothari <apoorv@toidiu.com>
2024-07-26 16:23:47 -0700

    refactor clock and test

--- src/clock.rs
+++ src/clock.rs
@@ -1,6 +1,11 @@
+use core::future::Future;
+use core::pin::Pin;
+use core::task::Context;
 use core::task::Poll;
 use core::time::Duration;
+use tokio::time::sleep_until;
 use tokio::time::Instant;
+use tokio::time::Sleep;
 
 // A monotonically increasing value
 pub struct Clock(Instant);
@@ -13,9 +18,9 @@ impl Default for Clock {
 
 impl Clock {
     fn elapsed(&self) -> Duration {
-        let e = self.0.elapsed();
-        println!("{:?}", e);
-        e
+        let elapsed = self.0.elapsed();
+        println!("elapsed: {:?}", elapsed);
+        elapsed
     }
 
     pub fn current_time(&self) -> Instant {
@@ -28,37 +33,39 @@ pub struct Timer {
     clock: Clock,
     // The Instant at which the timer should expire
     target: Option<Instant>,
+
+    sleep: Pin<Box<Sleep>>,
 }
 
 impl Timer {
     pub fn new(clock: Clock, target: Duration) -> Self {
         let target = clock.current_time() + target;
+        let sleep = Box::pin(sleep_until(target));
         Timer {
             clock,
             target: Some(target),
+            sleep,
         }
     }
 
-    pub fn is_ready(&self) -> bool {
+    fn is_ready(&mut self, ctx: &mut Context) -> bool {
+        dbg!("---{}", self.clock.elapsed());
+        let poll = self.sleep.as_mut().poll(ctx);
+
+        if poll.is_ready() {
+            self.target = None;
+        }
+
+        poll.is_ready()
+    }
+
+    pub fn sleep_until(&self) -> Option<Sleep> {
         if let Some(target) = self.target {
-            let expire_time = target.duration_since(self.clock.current_time());
-            if expire_time.is_zero() {
-                return true;
-            }
-        }
-
-        false
-    }
-}
-
-impl core::future::Future for Timer {
-    type Output = ();
-
-    fn poll(self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> Poll<Self::Output> {
-        if self.is_ready() {
-            Poll::Ready(())
+            let until = target + self.clock.elapsed();
+            dbg!("until {}", until);
+            Some(sleep_until(until))
         } else {
-            Poll::Pending
+            None
         }
     }
 }
@@ -66,18 +73,44 @@ impl core::future::Future for Timer {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use core::task::Waker;
+    use futures_test::task::new_count_waker;
     use tokio::time::sleep;
 
     #[tokio::test]
-    async fn timer_ready() {
+    async fn manual_check_elapsed_time() {
         let clock = Clock::default();
-        let timer = Timer::new(clock, Duration::from_secs(3));
-        assert!(!timer.is_ready());
+        let mut timer = Timer::new(clock, Duration::from_secs(2));
 
-        sleep(Duration::from_secs(2)).await;
-        assert!(!timer.is_ready());
+        let (waker, cnt) = new_count_waker();
+        let mut ctx = Context::from_waker(&waker);
+        assert!(!timer.is_ready(&mut ctx));
+        assert_eq!(cnt, 0);
+
+        // wait less than timer target
+        sleep(Duration::from_secs(1)).await;
+        assert!(!timer.is_ready(&mut ctx));
+        assert_eq!(cnt, 0);
 
         sleep(Duration::from_secs(1)).await;
-        assert!(timer.is_ready());
+        assert!(timer.is_ready(&mut ctx));
+        assert_eq!(cnt, 1);
+    }
+
+    #[tokio::test]
+    async fn sleep_until() {
+        let clock = Clock::default();
+        let mut timer = Timer::new(clock, Duration::from_secs(2));
+
+        let (waker, cnt) = new_count_waker();
+        let mut ctx = Context::from_waker(&waker);
+        assert!(!timer.is_ready(&mut ctx));
+        assert_eq!(cnt, 0);
+
+        timer.sleep_until().unwrap().await;
+        assert!(timer.is_ready(&mut ctx));
+        assert_eq!(cnt, 1);
+        // confirm that we did infact wait more than target seconds
+        assert!(timer.clock.elapsed() > Duration::from_secs(2));
     }
 }
