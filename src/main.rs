9bf6c842cad2093437d0af20d0e7f31514cfc874
Apoorv Kothari <apoorv@toidiu.com>
2024-07-26 18:14:08 -0700

    wip server interface

--- src/main.rs
+++ src/main.rs
@@ -1,94 +1,26 @@
 #![allow(unused)]
 
-use crate::clock::{Clock, Timer};
+use crate::server::Server;
+use crate::clock::{Clock};
 use core::time::Duration;
 use tokio::time::sleep;
 
+mod server;
 mod clock;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
     let clock = Clock::default();
-}
+    let mut server = Server::new(1);
 
-#[derive(Default)]
-enum State {
-    #[default]
-    Follower,
-    Leader,
-    Candidate,
-}
+    loop {
+        // recv Commands from peers.
+        server.recv();
 
-struct Follower {
-    heartbeat_recv_timeout: Clock,
-}
+        // check if timeout elapsed and attempt to make progress
+        server.on_timeout();
 
-struct Leader {
-    // ==== volatile state on leaders
-    // for each server, idx of next log entry to send to that server
-    next_idx: Vec<(CandidateId, usize)>,
-    // for each server, idx of highest log entry known to be replicated on server
-    match_idx: Vec<(CandidateId, usize)>,
-
-    heartbeat_send_timeout: Clock,
-}
-
-struct Candidate {
-    heartbeat_recv_timeout: Clock,
-}
-
-// A term is a logical clock
-struct Term(usize);
-
-struct CandidateId(usize);
-
-struct LogEntry {
-    term: Term,
-    command: Command,
-}
-
-enum Command {
-    // Leader election
-    RequestVote,
-
-    // Add entries and heartbeat
-    AppendEntries,
-}
-
-struct Server {
-    id: CandidateId,
-    state: State,
-
-    // ==== persistent state
-    current_term: Term,
-    voted_for: Option<CandidateId>,
-    log: Vec<LogEntry>,
-
-    // ==== volatile state
-    // idx of highest log entry known to be committed
-    commit_idx: usize,
-    // idx of the highest log entry applied to the state machine
-    last_applied: usize,
-}
-
-impl Server {
-    pub fn new(id: CandidateId) -> Self {
-        todo!()
-    }
-
-    fn poll(&mut self) {
-        match self.state {
-            State::Follower => todo!(),
-            State::Leader => todo!(),
-            State::Candidate => todo!(),
-        }
-    }
-
-    fn on_timeout(&mut self) {
-        match self.state {
-            State::Follower => todo!(),
-            State::Leader => todo!(),
-            State::Candidate => todo!(),
-        }
+        // TODO: recv client request
     }
 }
+
---
+++ src/server.rs
@@ -0,0 +1,85 @@
+use crate::clock::Clock;
+use core::{task::Context, time::Duration};
+use tokio::time::sleep;
+
+#[derive(Default)]
+enum State {
+    #[default]
+    Follower,
+    Leader,
+    Candidate,
+}
+
+struct Follower {
+    heartbeat_recv_timeout: Clock,
+}
+
+struct Leader {
+    // ==== volatile state on leaders
+    // for each server, idx of next log entry to send to that server
+    next_idx: Vec<(CandidateId, u64)>,
+    // for each server, idx of highest log entry known to be replicated on server
+    match_idx: Vec<(CandidateId, u64)>,
+
+    heartbeat_send_timeout: Clock,
+}
+
+struct Candidate {
+    heartbeat_recv_timeout: Clock,
+}
+
+// A term is a logical clock
+struct Term(u64);
+
+pub struct CandidateId(u64);
+
+struct LogEntry {
+    term: Term,
+    command: Command,
+}
+
+enum Command {
+    // Leader election
+    RequestVote,
+
+    // Add entries and heartbeat
+    AppendEntries,
+}
+
+pub struct Server {
+    id: CandidateId,
+    state: State,
+
+    // ==== persistent state
+    current_term: Term,
+    voted_for: Option<CandidateId>,
+    log: Vec<LogEntry>,
+
+    // ==== volatile state
+    // idx of highest log entry known to be committed
+    commit_idx: u64,
+    // idx of the highest log entry applied to the state machine
+    last_applied: u64,
+}
+
+impl Server {
+    pub fn new(id: u64) -> Self {
+        // get list of peers
+        // set timer
+
+        // initialize
+        todo!()
+    }
+
+    pub fn recv(&mut self) {
+        // check for received commands from the peer
+
+        todo!()
+    }
+
+    pub fn on_timeout(&mut self) {
+        // check timers
+
+        todo!()
+    }
+}
