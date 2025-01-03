11be4dd3ecf2338b26db7880dc8e895a645b0931
Apoorv Kothari <apoorv@toidiu.com>
2025-01-03 11:57:49 -0800

    add state from compliance

--- src/state.rs
+++ src/state.rs
@@ -29,3 +29,50 @@
 //!
 //! ```
 //! https://textik.com/#8dbf6540e0dd1676
+
+use crate::{
+    io::ServerTx,
+    log::{Idx, Log, Term, TermIdx},
+    rpc::{AppendEntries, RequestVote, Rpc},
+    server::ServerId,
+    timeout::Timeout,
+};
+use std::collections::HashSet;
+
+struct State {
+    //  ==== Persistent state on all servers ====
+    //% Compliance
+    //% `currentTerm` latest term server has seen (initialized to 0 on first boot, increases monotonically)
+    pub curr_term: Term,
+
+    //% Compliance
+    //% `votedFor` `candidateId` that received vote in current term (or null if none)
+    voted_for: Option<ServerId>,
+
+    //% Compliance
+    //% `log[]` log entries; each entry contains command for state machine, and term when entry was received by leader (first index is 1)
+    log: Log,
+
+    // ==== Volatile state on all servers ====
+    //% Compliance
+    //% `commitIndex` index of highest log entry known to be committed (initialized to 0, increases monotonically)
+    commit_idx: Idx,
+
+    //% Compliance
+    //% lastApplied: index of highest log entry applied to state machine (initialized to 0, increases monotonically)
+    last_applied: Idx,
+
+    // ==== Volatile state on leaders ====
+    //% Compliance
+    //% `nextIndex[]` for each server, index of the next log entry to send to that server (initialized to leader last log index + 1)
+
+    //% Compliance
+    //% `matchIndex[]` for each server, index of highest log entry known to be replicated on server (initialized to 0, increases monotonically)
+
+    // TODO
+    mode: Mode,
+    pub id: ServerId,
+    // list of all raft servers
+    server_list: Vec<ServerId>,
+    pub votes_received: HashSet<ServerId>,
+}
