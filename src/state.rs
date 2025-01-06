4e9bf778692f7c511bf78089051bec36f3601b07
Apoorv Kothari <apoorv@toidiu.com>
2025-01-05 22:35:03 -0800

    comment

--- src/state.rs
+++ src/state.rs
@@ -7,38 +7,38 @@ use std::collections::BTreeMap;
 
 pub struct State {
     //  ==== Persistent state on all servers ====
-    //% Compliance
+    //% Compliance:
     //% `currentTerm` latest term server has seen (initialized to 0 on first boot, increases
     //% monotonically)
     pub current_term: Term,
 
-    //% Compliance
+    //% Compliance:
     //% `votedFor` `candidateId` that received vote in current term (or null if none)
     pub voted_for: Option<ServerId>,
 
-    //% Compliance
+    //% Compliance:
     //% `log[]` log entries; each entry contains command for state machine, and term when entry was
     //% received by leader (first index is 1)
     log: Log,
 
     // ==== Volatile state on all servers ====
-    //% Compliance
+    //% Compliance:
     //% `commitIndex` index of highest log entry known to be committed (initialized to 0, increases
     //% monotonically)
     commit_idx: Idx,
 
-    //% Compliance
+    //% Compliance:
     //% lastApplied: index of highest log entry applied to state machine (initialized to 0,
     //% increases monotonically)
     last_applied: Idx,
 
     // ==== Volatile state on leaders ====
-    //% Compliance
+    //% Compliance:
     //% `nextIndex[]` for each server, index of the next log entry to send to that server
     //% (initialized to leader last log index + 1)
     next_idx: BTreeMap<ServerId, Idx>,
 
-    //% Compliance
+    //% Compliance:
     //% `matchIndex[]` for each server, index of highest log entry known to be replicated on server
     //% (initialized to 0, increases monotonically)
     match_idx: BTreeMap<ServerId, Idx>,
