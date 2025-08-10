2f23683041bc39ee9b838da8b906f58975c2031b
Apoorv Kothari <akothari@cloudflare.com>
2025-08-10 11:44:31 -0700

    refactor: cleanup testing

--- src/peer.rs
+++ src/peer.rs
@@ -1,8 +1,5 @@
 use crate::{io::ServerIO, server::ServerId};
 
-#[cfg(test)]
-mod testing;
-
 #[derive(Debug)]
 pub struct Peer<IO: ServerIO> {
     pub id: ServerId,
@@ -42,3 +39,28 @@ impl<IO: ServerIO> PartialEq for Peer<IO> {
 }
 
 impl<IO: ServerIO> Eq for Peer<IO> {}
+
+#[cfg(test)]
+mod testing {
+    use crate::{io::testing::MockIo, peer::Peer, server::ServerId};
+    use std::collections::BTreeMap;
+
+    impl Peer<MockIo> {
+        pub fn mock(fill: u8) -> (ServerId, Self) {
+            let id = ServerId::new([fill; 16]);
+            // FIXME pass in IO when this is used
+
+            (id, Self::new(id, MockIo::new()))
+        }
+
+        pub fn mock_as_map(ids_fill: &[u8]) -> BTreeMap<ServerId, Self> {
+            let mut map = BTreeMap::new();
+            for id in ids_fill {
+                let (id, peer) = Self::mock(*id);
+                map.insert(id, peer);
+            }
+
+            map
+        }
+    }
+}
